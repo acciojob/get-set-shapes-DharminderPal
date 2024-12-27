66f814bbb6b74f919492831b7d675790b94bc0a1
dharminder <144053986+DharminderPal@users.noreply.github.com>
2024-12-27 11:19:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
 class Rectangle {
-
-constructor(width, height ){
-this.width=width;
-this.height=height;
-}
-get width(){
-    return this.width;
-}
-
-get height(){
-return this.height;
-}
-
-getArea(){
-    return this.width*this.height ;
-}
-
-}
-class Square extends Rectangle{
-     constructor(side) {
-        super(side, side);
-}
-    getPerimeter(Square){
- return 4 * this.width; ;
+    constructor(width, height) {
+        this._width = width;  // Use a private variable to store width
+        this._height = height; // Use a private variable to store height
     }
- 
+
+    get width() {
+        return this._width; // Getter for width
+    }
+
+    get height() {
+        return this._height; // Getter for height
+    }
+
+    getArea() {
+        return this.width * this.height; // Calculate area
+    }
+
+    getPerimeter() {
+        return 2 * (this.width + this.height); // Calculate perimeter
+    }
 }
 
+class Square extends Rectangle {
+    constructor(side) {
+        super(side, side); // A square is a rectangle with equal width and height
+    }
+
+    getPerimeter() {
+        return 4 * this.width; // Perimeter of a square
+    }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
